fe87cc0ce065aaa6298448f95ead5ad028fe14e7
Sayanee <sayanee@gmail.com>
2019-05-03 14:49:15 +0800

    Add code to display harmful and not found ingredients

--- ewg.js
+++ ewg.js
@@ -1,29 +1,50 @@
 var osmosis = require('osmosis')
-var ingredientList = 'Water, Propylen Glycol, Ascorbic Acid , Hydroxyethylcellulose, Centella Asiatica Extract, Citrus Junos Fruit Extract , IlliciumVerum(Anise) Fruit Extract, Citrus Paradisi (Grapefruit) Fruit Extract, Nelumbium Speciosum Flower Extract, Paeonia Suffruticosa Root Extract , Scutellaria Baicalensis Root Extract, Polysorbate60, Brassica OleraceaItalica (Broccoli) Extract, Chaenomeles Sinensis Fruit Extract, Orange Oil Brazil, Sodium Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Disodium EDTA, Lavandula Angustifolia (Lavender) Oil , Camellia SinensisCallus Culture Extract, LarixEuropaeaWood Extract, Chrysanthellum Indicum Extract, Rheum Palmatum Root Extract, Asarum Sieboldi Root Extract, Quercus Mongolia Leaf Extract, PersicariaHydropiperExtract, Corydalis Turtschaninovii Root Extract, Coptis Chinensis Root Extract, Magnolia Obovata Bark Extract, Lysine HCL, Proline, Sodium Ascorbyl Phosphate, Acetyl Methionine, Theanine, Lecithin, Acetyl Glutamine, SH-Olgopeptide-1, SH-Olgopeptide-2, SH-Polypeptide-1, SH-Polypeptide-9, SH-Polypeptide-11, Bacillus/Soybean/Folic Acid Ferment Extract, Sodium Hyaluronate, CaprylylGlycol , Butylene Glycol, 1,2-Hexanediol'
-var ingredientListArray = ingredientList.split(',')
-
-console.log("Total number of ingredients: " + ingredientListArray.length)
+var ingredientList = 'Galactomyces Ferment Filtrate, Aqua (Water), Butylene Glycol, Pentylene Glycol, Glycerin, 1,2-Hexanediol, Bifida Ferment Lysate, Ceramide NP, Althaea Rosea Root Extract, Aloe Barbadensis Leaf Extract, Betaine, Panthenol, Allantoin, Sodium Hyaluronate, sh-Oligopeptide-1, Zanthoxylum, Piperitum Fruit Extract, Pulsatilla Koreana Extract Usnea Barbata (Lichen) Extract, Adenosine, Arginine, Xanthan Gum, Hydrogenated Lecithin, Polysorbate 20, Lecithin'
+var ingredientListArray = ingredientList.split(', ')
+var ingredientListNotFound = []
+var ingredientListHarmful = []
+var count = 0
 
 ingredientListArray.forEach(function(ingredient) {
   osmosis
   .get('https://www.ewg.org/skindeep/search.php?query=' + ingredient + '&h=Search')
-  .find('#table-browse > tr[2]')
+  .find('.prodwrapperrightcol2012')
   .set({
-      'ingredient':        'td[2]',
-      'score':  'td[3] > div > div > a > img@src'
+      'empty': '.rightside_content2012 p',
+      'ingredient': '#table-browse > tr[2] > td[2]',
+      'score': '#table-browse > tr[2] >td[3] > div > div > a > img@src'
   })
   .data(function(listing) {
-    var ingredientName = listing.ingredient.substring(0, listing.ingredient.indexOf('('))
-    var paragraph = listing.score
-    var regex = /image\d/g
-    var found = paragraph.match(regex)
-    var score = '\tSCORE: ' + found[0].replace(/image/g,'')
-
-    if (parseInt(score, 10) > 2) {
-      console.log(ingredientName + score)
+    if (listing.empty == 'There are no items in the database that match your request.') {
+      ingredientListNotFound.push(ingredient)
     }
+
+    if (listing.ingredient) {
+      var ingredientName = listing.ingredient.substring(0, listing.ingredient.indexOf('('))
+      var paragraph = listing.score
+      var regex = /image\d/g
+      var found = paragraph.match(regex)
+      var score = found[0].replace(/image/g, '')
+      score = parseInt(score, 10)
+
+      if (score > 2) {
+        console.log('For ' + ingredient + ', ' + ingredientName + ' score is ' + score + ' ❌')
+        ingredientListHarmful.push(listing.ingredient)
+      }
+    }
+
+    count++;
+    if (count == ingredientListArray.length ) {
+      console.log("---------------------------")
+      console.log('Total number of ingredients: ' + ingredientListArray.length)
+      console.log('Total number of ingredients harmful: ' + ingredientListHarmful.length)
+      console.log('Total number of ingredients not found in EWG DB: ' + ingredientListNotFound.length)
+      ingredientListNotFound.forEach(function(eachIngredient) {
+        console.log('\t' + eachIngredient)
+      })
+      console.log("---------------------------")
+    }
+
+    setTimeout(function() { }, 1000)
   })
-  // .log(console.log)
-  // .error(console.log)
-  // .debug(console.log)
 })
